1d7a448cbc1bcd62d1af7d8bb66b41985b60c6f3
Serj Miskiv <miskiv.sergei@steelkiwi.com>
2022-06-14 22:00:33 +0300

    Initial setup

---
+++ nanit_tech_assigment/nanit_tech_assigment/nanit_tech_assigmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  nanit_tech_assigmentApp.swift
+//  nanit_tech_assigment
+//
+//  Created by Serj Miskiv on 14.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct nanit_tech_assigmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
